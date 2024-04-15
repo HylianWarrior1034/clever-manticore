8dbecc4c07a48797516ec4f0a8d5c4de9704ae1a
Maria Movsheva <mmovshe1@jhu.edu>
2024-04-15 14:19:24 -0400

    Create TestChainShape.java

---
+++ jbox2d-library/src/test/java/SoftwareTestingUnitTests/TestChainShape.java
@@ -0,0 +1,12 @@
+package SoftwareTestingUnitTests;
+
+import org.jbox2d.collision.shapes.CircleShape;
+import org.jbox2d.common.Vec2;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestChainShape {
+}
